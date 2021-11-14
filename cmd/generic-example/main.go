f81d5bec8c4c5e8505c03a7e48501a32134eb46b
Le Tuan Anh <letuananh.ttmmt.ctu@gmail.com>
2021-11-14 13:21:31 +0700

    remove unnecessary  type args

--- cmd/generic-example/main.go
+++ cmd/generic-example/main.go
@@ -4,25 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Int value: ")
-	Print[int]([]int{1, 2, 3})
+	Print([]int{1, 2, 3})
 	fmt.Println()
 
 	fmt.Println("String value: ")
-	Print[string]([]string{"a", "b", "c"})
+	Print([]string{"a", "b", "c"})
 	fmt.Println()
 
 	fmt.Println("Float value: ")
-	Print[float32]([]float32{1.0, 2.1, 3.2})
+	Print([]float32{1.0, 2.1, 3.2})
 	fmt.Println()
 
 	fmt.Println("Bool value: ")
-	Print[bool]([]bool{true, false})
+	Print([]bool{true, false})
 	fmt.Println()
 }
 
 func Print[T any](s []T) {
 	for _, v := range s {
-		fmt.Print(v) 
+		fmt.Print(v)
 		fmt.Print("\t")
 	}
 }
